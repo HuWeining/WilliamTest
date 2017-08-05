bd8ad1d6621f04548c999399a509326b7e5506c1
LAPTOP-2603UC0B\huweining <huweining0330@gmail.com>
2017-08-05 15:26:40 +0800

    delete all

--- src/main/java/com/example/demo/config/WebSocketPath.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config;
-
-/**
- * Created by huweining on 2017/7/19.
- */
-public class WebSocketPath {
-    public static final String GUEST_PIPE = "/guest/update";
-}
--- src/main/java/com/example/demo/model/TravelPlanVO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-/**
- * Created by huweining on 2017/8/5.
- */
-public class TravelPlanVO {
-}
